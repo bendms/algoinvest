0c19df90941460fcdbe6cbe41b61e488798d5205
Benjamin Desmoulins <benjamin.desmoulins@gmail.com>
2022-09-28 11:21:52 +0200

    new version of algo with integration of dictionnaries much easier to test

--- bruteforce.py
+++ bruteforce.py
@@ -1,14 +1,57 @@
-from itertools import combinations
-wallet = 500
-actions_list = [20, 30, 50, 70, 60, 80, 22, 26, 48, 34, 42, 110, 38, 14, 18, 8, 4, 10, 24, 114]
-list_combinations = list()
+import csv
+import itertools
+list_of_actions_as_dict = []
+actions_name = []
+actions_value = []
+actions_profit = []
 compteur = 0
-for n in range(len(actions_list) + 1):
-    list_combinations += list(combinations(actions_list, n))
-for i in list_combinations:
-    if sum(i) < wallet:
-        total_of_investments = sum(i)
-        print(i, total_of_investments)
-        compteur += 1
-        print(compteur)
+compteur_2 = 0
+best_rentability = 0
+best_choice = []
+invest = 0
 
+
+with open ("dataset_bruteforce.csv", 'r') as csvfile:
+    """open the csv file and read it"""
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    next(csv_reader, None)
+    for row in csv_reader:
+        """for each row in the csv file, add the name of the action in the list actions_name"""
+        actions_name.append(row[0])
+        actions_value.append(row[1])
+        actions_profit.append(row[2])
+for action in actions_value:
+    """transform the list of string into a list of integer"""
+    action = int(action)
+for action in actions_profit:
+    """transform the list of string into a list of integer"""
+    action = int(action)
+for i in range(len(actions_name)):
+    """create a list of dictionary with actions name, value and profit"""
+    list_of_actions_as_dict.append({"name": actions_name[i], "value": int(actions_value[i]), "profit": float(actions_profit[i])/100})
+for i in range(len(list_of_actions_as_dict)):
+    """calculate the sellable value of each action and add in the dictionary"""
+    sellable_value = list_of_actions_as_dict[i]["value"] * (1+list_of_actions_as_dict[i]["profit"])
+    list_of_actions_as_dict[i]["sellable_value"] = sellable_value
+    print(list_of_actions_as_dict[i])
+for l in range(len(list_of_actions_as_dict) + 1):
+    """loop to create all length of combinations 0 -> 20 elements"""
+    for subset in itertools.combinations(list_of_actions_as_dict, l):
+        """for each subset of the list of actions, calculate the total value of the subset"""
+        total_value = 0
+        total_sellable_value = 0
+        for action in subset:
+            total_value += action["value"]
+            total_sellable_value += action["sellable_value"]
+        if total_value < 500:
+            """if the total value of the subset is less than 500, calculate the rentability"""
+            rentability = total_sellable_value - total_value
+            if rentability > best_rentability:
+                """if the rentability is better than the best rentability, change the best rentability and the best choice"""
+                best_rentability = rentability
+                best_choice = subset
+for actions in best_choice:
+    invest += actions["value"]
+    
+print(invest)
+print(best_rentability)
